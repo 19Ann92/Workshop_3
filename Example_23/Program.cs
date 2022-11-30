8d4b66a30af2e9d4991f02872075d5f2e3345c9b
Anna Eliseeva <Panterka6868@mail.ru>
2022-11-30 13:28:56 +0300

    add Example 23

---
+++ Example_23/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход 2 числа (N,M) и выдаёт 
+//таблицу кубов чисел от N до M.
+//2,3 -> 8, 27
+//1,5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.Write("Введите число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n < m)
+{
+    int count = n;
+
+    while (count < m)
+    {
+        Console.Write($"{count * count * count}, ");
+        count++;
+    }
+    Console.Write($"{m * m * m}");
+}
+
+else
+{
+    int count = m;
+
+    while (count < n)
+    {
+        Console.Write($"{count * count * count}, ");
+        count++;
+    }
+    Console.Write($"{n * n * n}");
+}
